1e984b9bf73f96fc015144a41618d19e6d67c967
divreg <wyatt.brege@gmail.com>
2012-06-11 14:26:43 -0700

    adding script

---
+++ move2mp3.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# Summary: Move directories containing mp3's to mp3 directory
+# Author: divreg
+
+# Specify base directory $1 
+if [ "$1" != "" ] ; then
+	cd $1 > /dev/null 2>&1
+fi
+
+# Specify mp3 directory $2
+if [ "$2" != "" ] ; then
+	mp3dir="$2"
+else mp3dir="/storage/music/mp3/"
+fi
+
+base=$( pwd )
+
+for directory in $(find $base -type d) ; do
+	cd $base
+	cd $directory > /dev/null 2>&1
+	files=$(ls *.[mM][Pp4][3a] > /dev/null 2>&1 | wc -l)
+	if [ $files != "0" ] ; then
+		cd $base
+		rsync -rt -v --delete --force $directory $mp3dir > /dev/null 2>&1
+#		cp -r "$directory" "$mp3dir"
+		echo "Moving $directory to $mp3dir"
+#		echo "Moving $directory to $mp3dir$directory"
+		continue
+	fi
+done
+
+echo "Complete!  Check $mp3dir for your new files!"
+
+exit 0
